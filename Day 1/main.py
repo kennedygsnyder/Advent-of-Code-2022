e09176c52d82ff7dc1dfc144cf69c75ab64b8f7a
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-06 20:00:59 -0500

    Part 1 Solution

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,4 +1,14 @@
+max_cals = 0
+curr_cals = 0
 with open('input.txt') as f:
     lines = f.readlines()
-    lines.split('\n\n')
-    print(lines)
+
+    for line in lines:
+        if line != '\n':
+            curr_cals += int(line)
+        else:
+            if curr_cals > max_cals:
+                max_cals = curr_cals
+            curr_cals = 0
+
+print(max_cals)
