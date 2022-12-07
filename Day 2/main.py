026c197f3a323f891c19131847ab09e7fa89aaeb
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-06 20:23:01 -0500

    Day 2 Part 2 Solution

--- Day 2/main.py
+++ Day 2/main.py
@@ -3,24 +3,29 @@ with open('input.txt') as f:
     lines = f.readlines()
     for line in lines:
         if (line[2] == 'X'):
-            score += 1
             if(line[0] =='A'):
                 score += 3
+            elif (line[0] == 'B'):
+                score += 1
             elif (line[0] == 'C'):
-                score += 6
+                score += 2
         
         elif (line[2] == 'Y'):
-            score += 2
-            if(line[0] =='B'):
+            score += 3
+            if(line[0] =='A'):
+                score += 1
+            elif (line[0] == 'B'):
+                score += 2
+            elif (line[0] == 'C'):
                 score += 3
-            elif (line[0] == 'A'):
-                score += 6
 
         elif (line[2] == 'Z'):
-            score += 3
-            if(line[0] =='C'):
-                score += 3
+            score += 6
+            if(line[0] =='A'):
+                score += 2
             elif (line[0] == 'B'):
-                score += 6
+                score += 3  
+            elif (line[0] == 'C'):
+                score += 1
 
 print(score)
