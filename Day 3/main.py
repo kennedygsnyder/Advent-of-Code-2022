712ee9f0af33617e27c619c6927a31db6890c490
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-08 00:10:39 -0500

    Day 3 Part 2 Solution

--- Day 3/main.py
+++ Day 3/main.py
@@ -2,10 +2,9 @@ sum = 0
 with open('input.txt') as f:
     lines = f.readlines()
 
-    for line in lines:
-        firstpart, secondpart = line[:len(line)//2], line[len(line)//2:]
-        for char in firstpart:
-            if char in secondpart:
+    for i in range(0,len(lines), 3):
+        for char in lines[i]:
+            if char in lines[i+1] and char in lines[i+2]:
                 num = ord(char)-96
                 if num < 0: num+=58
                 sum+=num
