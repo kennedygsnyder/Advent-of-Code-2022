9de2168f6e1cc865c6488cc26d8bc05710f888f4
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-13 17:00:36 -0500

    Day 4 Part 2 Solution

--- Day 4/main.py
+++ Day 4/main.py
@@ -7,10 +7,15 @@ with open('input.txt') as f:
         line = re.split(',|-', line.strip('\n'))
         line = [int(x) for x in line]
 
-        if line[0] <= line[2] and line[1] >= line[3]:
-            count+=1
-        elif line[0] >= line[2] and line[1] <= line[3]:
-            count +=1
+        if line[0] >= line[2] and line[0] <= line[3]:
+            count += 1
+        elif line[1] >= line[2] and line[1] <= line[3]:
+            count += 1
+        elif line[2] >= line[0] and line[2] <= line[1]:
+            count += 1
+        elif line[3] >= line[0] and line[3] <= line[1]:
+            count += 1
+
         
 print(count)
         
