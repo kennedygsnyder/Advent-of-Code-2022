d80f16c5998707df016e3c9de3b6ec54e119a294
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-13 18:30:23 -0500

    Day 13 Part 2 Solution

--- Day 13/main.py
+++ Day 13/main.py
@@ -47,6 +47,7 @@ def check(a, b):
 
 sum = 0
 curr_pair = 1
+packets_array = []
 
 with open('input.txt') as f:
     lines = f.readlines()
@@ -54,11 +55,42 @@ with open('input.txt') as f:
     for i in range(0, len(lines), 3):
         a = ast.literal_eval(lines[i])
         b = ast.literal_eval(lines[i + 1])
+        packets_array.append(a)
+        packets_array.append(b)
+
         if check(a,b) == 1:
             sum+=curr_pair
-            print(curr_pair)
         
         curr_pair +=1
 
-print(sum)
-    
+    print(sum)
+
+    #PART 2
+    def bubble_sort(array):
+        n = len(array)
+
+        for i in range(n):
+            already_sorted = True
+
+            for j in range(n - i - 1):
+                if check(array[j], array[j + 1]) == -1:
+                    array[j], array[j + 1] = array[j + 1], array[j]
+                    already_sorted = False
+
+            if already_sorted:
+                break
+
+        return array
+
+    packets_array.append([[2]])
+    packets_array.append([[6]])
+
+    divider_index_sum = 1
+    sorted_array = bubble_sort(packets_array)
+    for i in range(len(sorted_array)):
+        if sorted_array[i] == [[2]] or sorted_array[i] == [[6]]:
+            divider_index_sum *= i+1
+
+    print(divider_index_sum)
+
+
