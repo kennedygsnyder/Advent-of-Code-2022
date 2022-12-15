d5bdc0dc6a1638a2ac2c4e6392c187885c3d9032
kennedygsnyder <94766977+kennedygsnyder@users.noreply.github.com>
2022-12-15 17:57:49 -0500

    Day 15 Part 2 Solution

--- Day 15/main.py
+++ Day 15/main.py
@@ -23,11 +23,12 @@ for line in lines:
     sensor_position.append(manhattan_distance(sensor_position, beacon_position))
     sensors.append(sensor_position)
 
-y = 10
+y = 2000000
 count = -1
 not_beacon = False
 
-for x in range(min_x-1500000, max_x+1500000, 1):
+# Part 1
+"""for x in range(min_x-1500000, max_x+1500000, 1):
     for sensor in sensors:
         if manhattan_distance([x, y], [sensor[0], sensor[1]]) <= sensor[2]:
             not_beacon = True
@@ -38,6 +39,67 @@ for x in range(min_x-1500000, max_x+1500000, 1):
 
     not_beacon = False
 
-print(count)
+print(count)"""
 
+
+# Part 2
+#walk around perimeter of every sensor to get all possible points
+points = []
+count = 1
+
+for sensor in sensors:
+    print(str(count) + '/33')
+    x, y, dist = sensor
+
+    # starting point on top of diamond
+    point_a = [x, y-dist-1]
+    #rightmost point of diamond
+    point_b = [x + dist + 1, y]
+    #bottommost point of diamond
+    point_c = [x, y + dist + 1]
+    #leftmost point
+    point_d = [x - dist - 1, y]
+
+    curr_point = point_a
+    # top right edge
+    while curr_point[0] < point_b[0] and curr_point[1] < point_b[1]:
+        points.append([curr_point[0], curr_point[1]])
+        curr_point[0] += 1
+        curr_point[1] += 1
+
+    #bottom right edge
+    while curr_point[0] > point_c[0] and curr_point[1] < point_c[1]:
+        points.append([curr_point[0], curr_point[1]])
+        curr_point[0] -= 1
+        curr_point[1] += 1
+
+    #bottom left edge
+    while curr_point[0] > point_d[0] and curr_point[1] > point_d[1]:
+        points.append([curr_point[0], curr_point[1]])
+        curr_point[0] -= 1
+        curr_point[1] -= 1
+
+    #top left edge
+    while curr_point[0] < point_a[0] and curr_point[1] > point_a[1]:
+        points.append([curr_point[0], curr_point[1]])
+        curr_point[0] += 1
+        curr_point[1] -= 1
+
+    points.append([curr_point[0], curr_point[1]])
+
+    count+=1
+
+not_beacon = False
+for point in points:
+    if point[0] < 4000000 and point[0] > 0 and point[1] < 4000000 and point[1] > 0:
+        for sensor in sensors:
+            if manhattan_distance([point[0], point[1]], [sensor[0], sensor[1]]) <= sensor[2]:
+                not_beacon = True
+                break
+    
+        if not not_beacon:
+            print(point[0]*4000000+point[1])
+            break
+
+    not_beacon = False
 
